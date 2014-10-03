4c88c000c22cb5acf275d92dc320bb6c95d239ef
LordCopper <acoppero@gmail.com>
2014-10-03 14:01:00 +0200

    sista rättningen

--- AllaBolagScrubber/ScreenScraperLib/Class1.cs
+++ AllaBolagScrubber/ScreenScraperLib/Class1.cs
@@ -57,7 +57,7 @@ namespace ScreenScraperLib
 
             foreach (var name in nodes)
             {
-                orgName = "Från AllaBolag.se " + name.InnerText;
+                orgName = "Från Allabolag.se " + name.InnerText;
             }
 
             return orgName;
