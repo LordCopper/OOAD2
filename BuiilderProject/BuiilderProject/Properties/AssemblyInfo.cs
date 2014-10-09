9046fb2e9eb1a718522bc68a2d1c2f0007cae066
LordCopper <acoppero@gmail.com>
2014-10-09 10:26:07 +0200

    unittest project

--- BuiilderProject/BuiilderProject/Properties/AssemblyInfo.cs
+++ BuiilderProject/BuiilderProject/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MicrosoftScruber")]
+[assembly: AssemblyTitle("BuiilderProject")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MicrosoftScruber")]
+[assembly: AssemblyProduct("BuiilderProject")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("55fd6dd3-3dc5-4c30-b1fb-cdebd2a1673c")]
+[assembly: Guid("fb00f3a3-22f0-419c-8125-68abd0480249")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,7 +29,8 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
