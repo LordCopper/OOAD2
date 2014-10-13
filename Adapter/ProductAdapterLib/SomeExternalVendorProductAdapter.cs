a6af76e47b794d3981c5004260bdffac097fe148
Tempest91 <tempestnorlin@gmail.com>
2014-10-13 14:42:01 +0200

    yt

---
+++ Adapter/ProductAdapterLib/SomeExternalVendorProductAdapter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary;
+
+namespace ProductAdapterLib
+{
+
+    public class SomeExternalVendorProductAdapter : IProduct
+    {
+
+        public SomeExternalVendorProductAdapter(SomeExternalVendorProduct someExternalVendorProduct)
+
+        {
+           
+        }
+
+        public decimal UnitPrice
+        {
+            get { return 20; }
+        }
+
+        public string BarCode
+        {
+            get { return "12345"; }
+        }
+
+        public string Title
+        {
+            get { return "Ost"; }
+        }
+    }
+}
+
