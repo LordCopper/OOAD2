ab79fdd237b1723a0c318197d214aeae6597775a
Tempest91 <tempestnorlin@gmail.com>
2014-10-13 13:39:23 +0200

    fixat ett nytt unittest

---
+++ Adapter/UnitTestForAdapter/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestForAdapter
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestProduct()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestSomeExternalVendorProduct()
+        {
+
+        }
+    }
+}
