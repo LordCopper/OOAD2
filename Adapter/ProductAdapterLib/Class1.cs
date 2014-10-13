e082c72caf1ee0d326d7a2c8ac118a9d54744a08
Tempest91 <tempestnorlin@gmail.com>
2014-10-13 13:14:08 +0200

    uppdateringar

--- Adapter/ProductAdapterLib/Class1.cs
+++ Adapter/ProductAdapterLib/Class1.cs
@@ -4,10 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ClassLibrary
+namespace ProductAdapterLib
 {
-    public abstract class Class1
+    public class Class1
     {
-   
     }
 }
